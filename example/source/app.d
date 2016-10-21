8d701e34a417a43e6b2a68f6e4bb162278f036dd
Christian <asistentedeprincipios2@gmail.com>
2016-10-21 17:25:36 -0600

    Cambia puerto para conexion

--- example/source/app.d
+++ example/source/app.d
@@ -7,8 +7,8 @@ import vibe.d;
 shared static this()
 {
     auto settings = new HTTPServerSettings;
-    settings.port = 8080;
+    settings.port = environment [ "PORT" ].to!ushort;
     settings.bindAddresses = ["::1", "127.0.0.1"];
     listenHTTP(settings, &hello);
-    logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+    logInfo("Sleep.");
 }
